31acfd050194056484149ad6e551a5ce479b0fb9
Nikola Andrejevic <nikola.andrejevic@mail.utoronto.ca>
2019-01-22 18:19:11 -0500

    Revert to empty

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/hmnik/Documents/catkin_ws/src
-# Build directory: /mnt/c/Users/hmnik/Documents/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/mnt/c/Users/hmnik/Documents/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/mnt/c/Users/hmnik/Documents/catkin_ws/devel;/opt/ros/melodic".split(';') if "/mnt/c/Users/hmnik/Documents/catkin_ws/devel;/opt/ros/melodic" != "" else []
