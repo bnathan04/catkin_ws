e4dfa04f8bbf100c4827d6f612221ce7f7db558d
Balu Nathan <balu.nathan.04@gmail.com>
2019-03-05 17:09:04 -0500

    WIP: Compute target positions for each box (NOT COMPILED).

--- src/mie443_contest2/src/contest2.cpp
+++ src/mie443_contest2/src/contest2.cpp
@@ -3,6 +3,62 @@
 #include <robot_pose.h>
 #include <imagePipeline.h>
 
+/* Our includes */
+#include <cmath.h>
+
+/* Params */
+float distanceFromBox = 2.50;
+
+
+std::vector<vector<float>> computeTarget(Boxes) {
+
+    std::vector<vector<float>> result;
+
+    for(int i = 0; i < boxes.coords.size(); ++i) {
+        float x = boxes.coords[i][0];
+        float y = boxes.coords[i][1];
+        float phi = boxes.coords[i][2] * 180.0 / M_PI;
+
+        float xOffset = 0.0;
+        float yOffset = 0.0;
+
+        if (phi <= 90.0 && phi > 0.0) {
+            xOffset = distanceFromBox * cos(phi);
+            yOffset = distanceFromBox * sin(phi);
+        } 
+
+        else if (phi <= 180.0 && phi > 90.0) {
+            xOffset = distanceFromBox * sin(phi-90.0) * -1.0;
+            yOffset = distanceFromBox * cos(phi-90.0);
+        }
+
+        else if (phi <=270.0 && phi >  180.0) {
+            xOffset = -1.0 * distanceFromBox * cos(phi-180.0);
+            yOffset = -1.0 * distanceFromBox * sin(phi-180.0);
+        }
+
+        else {
+            xOffset = distanceFromBox * sin(phi-270.0);
+            yOffset = -1.0 * distanceFromBox * cos(phi-270.0);
+        }  
+           
+        float xTarget = x + xOffset;
+        float yTarget = y + yOffset;
+        float phiTarget = ((phi + 180)%360) * M_PI / 180.0 ;
+
+        vector<float> newCoords;
+        newCoords.push_back(xTarget);
+        newCoords.push_back(yTarget);
+        newCoords.push_back(phiTarget);
+
+        result.push_back(newCoords)
+    }
+
+    return result;
+
+}
+
+
 int main(int argc, char** argv) {
     // Setup ROS.
     ros::init(argc, argv, "contest2");
@@ -24,11 +80,19 @@ int main(int argc, char** argv) {
     // Initialize image objectand subscriber.
     ImagePipeline imagePipeline(n);
     // Execute strategy.
+
+    std::vector<vector<float>> targetPoints = computeTarget(Boxes);
+
+
     while(ros::ok()) {
         ros::spinOnce();
         /***YOUR CODE HERE***/
         // Use: boxes.coords
         // Use: robotPose.x, robotPose.y, robotPose.phi
+
+
+
+
         imagePipeline.getTemplateID(boxes);
         ros::Duration(0.01).sleep();
     }
